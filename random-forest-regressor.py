fb4b29f974369487b266027d9f52dea5746fc905
Alexander Tan WT <alexandertan950512@yahoo.com>
2024-04-18 00:28:55 +0800

    1. Added random forest regressor

---
+++ random-forest-regressor.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import yaml
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor  # Import KNeighborsRegressor
+from sklearn import metrics
+
+with open('config.yml', 'r') as f:
+    dictConfig = yaml.load(f, Loader=yaml.FullLoader)
+
+customers = pd.read_csv(f"{dictConfig['path']['data']}Ecommerce_Customers.csv", index_col=0)
+print(customers.head(5))
+
+y = customers['Yearly Amount Spent']
+X = customers[['Avg. Session Length', 'Time on App', 'Time on Website', 'Length of Membership']]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+depthRf = 4
+numTreeRf = 15
+
+model = RandomForestRegressor(max_depth = depthRf, n_estimators = numTreeRf)
+model.fit(X_train, y_train)
+
+# Predictions
+predictions = model.predict(X_test)
+plt.scatter(y_test, predictions)
+plt.xlabel('Y Test')
+plt.ylabel('Predicted Y')
+plt.show()
+
+# Evaluate model
+print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+
+
+
+
+
+
